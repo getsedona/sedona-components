ed85ae4453197de46577d654f9fc389328dbefe1
Maxim Sofronov <imaxsof@gmail.com>
2019-02-08 12:31:04 +0300

    fix(js): table

--- src/table/index.js
+++ src/table/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let toggleShadow = el => {
+let toggleShadow = (el) => {
 	if (el.scrollWidth - el.scrollLeft <= el.clientWidth) {
 		el.parentElement.classList.remove("table--shadow-right");
 	}
@@ -16,14 +16,14 @@ let toggleShadow = el => {
 	if (el.scrollLeft > 0) {
 		el.parentElement.classList.add("table--shadow-left");
 	}
-}
+};
 
 export function table() {
 	if (!document.querySelector(".table .table__wrap")) {
 		return;
 	}
 
-	document.querySelectorAll(".table__wrap").forEach(el => {
+	document.querySelectorAll(".table__wrap").forEach((el) => {
 		if (el.parentElement.classList.contains("table-js-init")) {
 			return;
 		}
