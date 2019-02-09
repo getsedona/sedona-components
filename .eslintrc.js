5cd433b5942c4d6a6717b51d7fc51045ccb7e5ca
Maxim Sofronov <imaxsof@gmail.com>
2019-02-09 16:58:16 +0300

    refactor: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 "use strict";
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
-    parser: "babel-eslint"
+  "root": true,
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "parser": "babel-eslint"
   },
-  env: {
-    es6: true,
-    node: true,
-    browser: true
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true
   },
-  extends: [
+  "extends": [
     "eslint:recommended"
   ],
-  rules: {
+  "rules": {
     "arrow-parens": ["error", "always"],
     "comma-dangle": ["error", "always-multiline"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"]
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"]
   }
 }
