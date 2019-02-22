ba60b86ddd79ab2612c255a0e80e279a1c4caffb
Maxim Sofronov <imaxsof@gmail.com>
2019-02-22 11:50:46 +0300

    chore: обновлен stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-  "extends": "stylelint-config-sedona",
-  "rules": {
-    "comment-empty-line-before": "always",
-    "comment-whitespace-inside": "always",
-    "max-empty-lines": 1,
-    "indentation": ["tab", {
-      "baseIndentLevel": 1
-    }],
-    "no-empty-first-line": true
-  }
+  "extends": "stylelint-config-sedona"
 }
