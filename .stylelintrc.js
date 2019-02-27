8746fec7d1ffbf5c2e2f18d225370488f3b888aa
Maxim Sofronov <imaxsof@gmail.com>
2019-02-27 10:11:31 +0300

    chore: update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  "extends": "stylelint-config-sedona"
+  "extends": "stylelint-config-sedona",
+  "rules": {
+    "selector-max-id": 2,
+    "color-no-invalid-hex": null,
+  }
 }
