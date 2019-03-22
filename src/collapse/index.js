50a726aa081a73859752a1ee79bf7b4a02944525
frymmox <frymmox@gmail.com>
2019-03-22 14:32:48 +0500

    feature: collapse

--- src/collapse/index.js
+++ src/collapse/index.js
@@ -1,3 +1,27 @@
+const toggleCollapse = (selector, cmd) => {
+  const fnmap = {
+    "toggle": "toggle",
+    "show": "add",
+    "hide": "remove"
+  };
+
+  const targets = Array.from(document.querySelectorAll(selector));
+
+  targets.forEach(target => {
+    target.classList[fnmap[cmd]]("show");
+  });
+}
+
 export function collapse() {
-  return;
+  const triggers = Array.from(document.querySelectorAll("[data-toggle='collapse']"));
+
+  window.addEventListener("click", (event) => {
+    const element = event.target;
+
+    if (triggers.includes(element)) {
+      const selector = element.getAttribute("data-target");
+
+      toggleCollapse(selector, "toggle");
+    }
+  }, false);
 }
