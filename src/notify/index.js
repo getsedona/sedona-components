1ff731cdc9419a0d29ccb2a5c689cb525ae63f44
frymmox <frymmox@gmail.com>
2019-03-22 16:45:08 +0500

    feature: notify

--- src/notify/index.js
+++ src/notify/index.js
@@ -1,3 +1,13 @@
-export function notify() {
-  return;
+const defaultParams = {
+  selector: ".js-notify-close",
+  hideClass: "notify--hide"
+};
+
+export function notify(params = defaultParams) {
+  const element = document.querySelector(params.selector);
+  if (element !== null) {
+    element.addEventListener("click", function() {
+      document.querySelector(".notify").classList.add(params.hideClass);
+    });
+  }
 }
