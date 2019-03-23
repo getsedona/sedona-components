6e7ec2d31e253db6ae52be7ea8a6524ec7e6d078
Maxim Sofronov <imaxsof@gmail.com>
2019-03-23 10:23:49 +0300

    chore: fix linting

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   "extends": "stylelint-config-sedona",
   "rules": {
-    "selector-max-id": 2,
-    "color-no-invalid-hex": null,
     "indentation": [2, {
       "baseIndentLevel": 1
     }],
