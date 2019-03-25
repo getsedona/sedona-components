35ac66526202defe2283b9eb0f2f5db2b1793d13
Maxim Sofronov <imaxsof@gmail.com>
2019-03-25 11:35:31 +0300

    fix: collapse

--- src/collapse/index.js
+++ src/collapse/index.js
@@ -1,27 +1,13 @@
-const toggleCollapse = (selector, cmd) => {
-  const fnmap = {
-    "toggle": "toggle",
-    "show": "add",
-    "hide": "remove"
-  };
-
-  const targets = Array.from(document.querySelectorAll(selector));
-
-  targets.forEach(target => {
-    target.classList[fnmap[cmd]]("show");
-  });
-}
-
 export function collapse() {
-  const triggers = Array.from(document.querySelectorAll("[data-toggle='collapse']"));
+  if (!document.querySelector("[data-toggle='collapse']")) {
+    return;
+  }
 
-  window.addEventListener("click", (event) => {
-    const element = event.target;
+  document.querySelectorAll("[data-toggle='collapse']").forEach((el) => {
+    el.addEventListener("click", () => {
+      const selector = el.getAttribute("data-target");
 
-    if (triggers.includes(element)) {
-      const selector = element.getAttribute("data-target");
-
-      toggleCollapse(selector, "toggle");
-    }
-  }, false);
+      document.querySelector(selector).classList.toggle("collapse--show");
+    });
+  });
 }
