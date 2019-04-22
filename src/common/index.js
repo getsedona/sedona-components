cfd74ce0483ef3c13dce1fcc944cabd3e1f78b9c
Maxim Sofronov <imaxsof@gmail.com>
2019-04-22 18:02:24 +0300

    fix: рассчитана кастомная переменная для определения высоты вьюпорта

--- src/common/index.js
+++ src/common/index.js
@@ -5,3 +5,13 @@ const md = new MobileDetect(window.navigator.userAgent);
 if (md.mobile()) {
   document.body.classList.add("mobile");
 }
+
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
