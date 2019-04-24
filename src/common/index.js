c6b866b8ae149b907d9a147a03321695fbb45c9a
Maxim Sofronov <imaxsof@gmail.com>
2019-04-24 10:28:03 +0300

    fix(js): common

--- src/common/index.js
+++ src/common/index.js
@@ -11,7 +11,7 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
+  vh = window.innerHeight * 0.01;
 
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
