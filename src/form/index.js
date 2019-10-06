7cef25b42f85ce03d05ea42597826c47f422842b
Maxim Sofronov <imaxsof@gmail.com>
2019-10-06 15:30:30 +0300

    wip: подготовка к версии 4.10.0

--- src/form/index.js
+++ src/form/index.js
@@ -1,35 +1,33 @@
 export function form() {
-  if (!document.querySelector(".form--slip")) {
+  if (!document.querySelector(".form__item--slip")) {
     return;
   }
 
-  document.querySelectorAll(".form--slip").forEach((el) => {
-    if (el.classList.contains("js-form-slip-init")) {
+  document.querySelectorAll(".form__item--slip").forEach((el) => {
+    if (el.classList.contains("js-form-item-slip-init")) {
       return;
     }
 
-    el.classList.add("js-form-slip-init");
+    el.classList.add("js-form-item-slip-init");
 
-    el.querySelectorAll(".form__item").forEach((item) => {
-      item.addEventListener("focusin", (evt) => {
-        const targetEl = evt.target;
+    el.addEventListener("focusin", (evt) => {
+      const targetEl = evt.target;
 
-        if (targetEl.classList.contains("form__input") || targetEl.classList.contains("form__textarea")) {
-          item.classList.add("form__item--slip");
+      if (targetEl.classList.contains("form__input") || targetEl.classList.contains("form__textarea")) {
+        el.classList.add("form__item--slippery");
+      }
+    });
+
+    el.addEventListener("focusout", (evt) => {
+      const targetEl = evt.target;
+
+      if (targetEl.classList.contains("form__input") || targetEl.classList.contains("form__textarea")) {
+        if (targetEl.value) {
+          el.classList.add("form__item--slippery");
+        } else {
+          el.classList.remove("form__item--slippery");
         }
-      });
-
-      item.addEventListener("focusout", (evt) => {
-        const targetEl = evt.target;
-
-        if (targetEl.classList.contains("form__input") || targetEl.classList.contains("form__textarea")) {
-          if (targetEl.value) {
-            item.classList.add("form__item--slip");
-          } else {
-            item.classList.remove("form__item--slip");
-          }
-        }
-      });
+      }
     });
   });
 }
