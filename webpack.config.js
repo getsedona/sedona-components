0743b6ee4664bad33d2f799351be580c06d12848
Maxim Sofronov <imaxsof@gmail.com>
2020-06-29 19:49:50 +0300

    add: price component

--- webpack.config.js
+++ webpack.config.js
@@ -83,6 +83,11 @@ module.exports = {
       minify: isProduction ? htmlMinifyConfig : false,
     }),
     new HtmlWebpackPlugin({
+      template: "./dev/pages/prices.pug",
+      filename: "prices.html",
+      minify: isProduction ? htmlMinifyConfig : false,
+    }),
+    new HtmlWebpackPlugin({
       layout: path.join(__dirname, "dev/layouts/default.html"),
       template: "./src/base/examples.html",
       filename: "base.html",
