672e4e1967796d314cf41fc521b71f5a4f83eea3
Maxim Sofronov <imaxsof@gmail.com>
2020-07-02 12:48:27 +0300

    chore: fix

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,6 +2,7 @@ const isProduction = process.env.NODE_ENV === "production" ? true : false;
 
 module.exports = {
   plugins: [
+    require("postcss-css-variables")({ preserve: true }),
     require("autoprefixer"),
     isProduction ? require("cssnano")({ preset: "default" }) : null,
   ],
