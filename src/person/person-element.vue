ae2a4c091ba9c8df0bcbb48bbe31d9d18c308ea3
Maxim Sofronov <imaxsof@gmail.com>
2020-07-05 21:33:10 +0300

    add: vie components person & quote

---
+++ src/person/person-element.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .person(:class="{ 'person--gravity': person.isGravity }")
+    .person__avatar(
+      v-if="person.avatar"
+      :style="`width: ${person.avatar.size}px; height: ${person.avatar.size}px;`")
+      img.person__image(
+        :src="person.avatar.src"
+        :width="person.avatar.size"
+        :height="person.avatar.size"
+        alt="")
+
+    .person__content(v-if="person.name || person.note")
+      .person__name(v-if="person.name") {{ person.name }}
+      .person__note(v-if="person.note") {{ person.note }}
+</template>
+
+<script>
+  export default {
+    name: 'person-element',
+    props: {
+      person: {
+        type: Object,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style lang="less">
+  @import "./index";
+</style>
---
+++ src/quote/quote-element.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  blockquote.quote(
+    :lang="quote.lang"
+    :cite="quote.cite")
+    .quote__main {{ quote.content }}
+
+    footer.quote__footer(v-if="person")
+      .quote__divider
+      person-element(:person="person")
+</template>
+
+<script>
+  import PersonElement from './../person/person-element'
+
+  export default {
+    name: 'quote-element',
+    components: {
+      PersonElement,
+    },
+    props: {
+      quote: {
+        type: Object,
+        required: true,
+      },
+      person: {
+        type: Object,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style lang="less">
+  @import "./index";
+</style>
