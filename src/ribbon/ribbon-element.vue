311e48ed231ec2da7aec8faa5a01ea2037f74bf2
Maxim Sofronov <imaxsof@gmail.com>
2020-07-12 22:15:31 +0300

    add: ribbon vue component

---
+++ src/ribbon/ribbon-element.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .ribbon
+    .ribbon__box
+      .ribbon__value {{ value }}
+</template>
+
+<script>
+  export default {
+    name: 'ribbon-element',
+    props: {
+      value: {
+        type: String,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style lang="less">
+  @import "./index";
+</style>
